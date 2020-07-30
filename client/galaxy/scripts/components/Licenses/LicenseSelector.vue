65162f131192a2a4ef837915f2d580e0c59eb932
assuntad23 <assuntad23@gmail.com>
2020-07-30 19:15:28 -0400

    adding folding structure

---
+++ client/galaxy/scripts/components/Licenses/LicenseSelector.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="btn-group dropdown">
+        <button
+            type="button"
+            v-b-tooltip.hover.bottom
+            :title="titleFilterMenu"
+            class="rule-menu-filter-button primary-button dropdown-toggle"
+            data-toggle="dropdown"
+        >
+            <span class="fa fa-plus"></span>
+            {{ l("Filter") }}
+            <span class="caret"></span>
+        </button>
+        <div class="dropdown-menu" role="menu">
+            <rule-target-component :builder="this" rule-type="add_filter_regex" />
+            <rule-target-component :builder="this" rule-type="add_filter_matches" />
+            <rule-target-component :builder="this" rule-type="add_filter_compare" />
+            <rule-target-component :builder="this" rule-type="add_filter_empty" />
+            <rule-target-component :builder="this" rule-type="add_filter_count" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
--- client/galaxy/scripts/components/Workflow/Editor/Attributes.vue
+++ client/galaxy/scripts/components/Workflow/Editor/Attributes.vue
@@ -15,11 +15,6 @@
                 </b-form-select-option>
             </b-form-select>
         </div>
-        <div id="license-selector">
-            <b>License</b>
-            <div class="dropdown-menu" role="menu">
-            </div>
-        </div>
         <div v-if="hasParameters" id="workflow-parameters-area" class="mt-2">
             <b>Parameters</b>
             <b-list-group>
